6a0dad61ae96ee862edb50abc86a76d2595e310b
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-06-15 18:10:20 +0700

    fix(config): change to use file

--- .config/bspwm/rices/xcad/theme.sh
+++ .config/bspwm/rices/xcad/theme.sh
@@ -29,37 +29,7 @@ set_term_config() {
         -e "s/family: .*/family: Hack Nerd Font/g" \
         -e "s/size: .*/size: 10/g"
 
-    cat > "$HOME"/.config/alacritty/colors.yml <<- _EOF_
-        # Colors (Xcad)
-        colors:
-          primary:
-            background: '#1B1B1B'
-            foreground: '#F1F1F1'
-
-          normal:
-            black: '#121212'
-            red: '#A52AFF'
-            green: '#7129FF'
-            yellow: '#3D2AFF'
-            blue: '#2B4FFF'
-            magenta: '#2883FF'
-            cyan: '#28B9FF'
-            white: '#F1F1F1'
-
-          bright:
-            black: '#666666'
-            red: '#BA5AFF'
-            green: '#905AFF'
-            yellow: '#685AFF'
-            blue: '#5C78FF'
-            magenta: '#5EA2FF'
-            cyan: '#5AC8FF'
-            white: '#FFFFFF'
-
-          cursor:
-            cursor: '#FFFFFF'
-            text: '#1B1B1B'
-	_EOF_
+    cat "$HOME"/.dotfiles/.config/bspwm/rices/xcad/colors.yml > "$HOME"/.config/alacritty/colors.yml
 }
 
 # Set compositor configuration
@@ -82,22 +52,7 @@ set_dunst_config() {
         -e "s/foreground='.*'/foreground='#2883FF'/g"
 
     sed -i '/urgency_low/Q' "$HOME"/.config/bspwm/dunstrc
-    cat >> "$HOME"/.config/bspwm/dunstrc <<- _EOF_
-        [urgency_low]
-        timeout = 3
-        background = "#1B1B1B"
-        foreground = "#F1F1F1"
-
-        [urgency_normal]
-        timeout = 6
-        background = "#1B1B1B"
-        foreground = "#F1F1F1"
-
-        [urgency_critical]
-        timeout = 0
-        background = "#1B1B1B"
-        foreground = "#F1F1F1"
-	_EOF_
+    cat "$HOME"/.dotfiles/.config/bspwm/rices/xcad/dunstrc >> "$HOME"/.config/bspwm/dunstrc
 }
 
 # Launch the bar
