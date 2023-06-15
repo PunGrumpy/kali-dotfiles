2176bb4fd3c4fadd4b6e72b713f55bfe53ffb89f
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-06-15 18:06:52 +0700

    fix(config): syntax

--- .config/bspwm/rices/xcad/theme.sh
+++ .config/bspwm/rices/xcad/theme.sh
@@ -25,86 +25,86 @@ set_bspwm_config() {
 
 # Reload terminal colors
 set_term_config() {
-		sed -i "$HOME"/.config/alacritty/fonts.yml \
-		-e "s/family: .*/family: Hack Nerd Font/g" \
-		-e "s/size: .*/size: 10/g"
+    sed -i "$HOME"/.config/alacritty/fonts.yml \
+        -e "s/family: .*/family: Hack Nerd Font/g" \
+        -e "s/size: .*/size: 10/g"
 
-		cat > "$HOME"/.config/alacritty/colors.yml <<- _EOF_
-				# Colors (Xcad)
-				colors:
-				  primary:
-				    background: '#1B1B1B'
-					foreground: '#F1F1F1'
+    cat > "$HOME"/.config/alacritty/colors.yml <<- _EOF_
+        # Colors (Xcad)
+        colors:
+          primary:
+            background: '#1B1B1B'
+            foreground: '#F1F1F1'
 
-				  normal:
-				    black: '#121212'
-					red: '#A52AFF'
-					green: '#7129FF'
-					yellow: '#3D2AFF'
-					blue: '#2B4FFF'
-					magenta: '#2883FF'
-					cyan: '#28B9FF'
-					white: '#F1F1F1'
+          normal:
+            black: '#121212'
+            red: '#A52AFF'
+            green: '#7129FF'
+            yellow: '#3D2AFF'
+            blue: '#2B4FFF'
+            magenta: '#2883FF'
+            cyan: '#28B9FF'
+            white: '#F1F1F1'
 
-				  bright:
-				    black: '#666666'
-					red: '#BA5AFF'
-					green: '#905AFF'
-					yellow: '#685AFF'
-					blue: '#5C78FF'
-					magenta: '#5EA2FF'
-					cyan: '#5AC8FF'
-					white: '#FFFFFF'
+          bright:
+            black: '#666666'
+            red: '#BA5AFF'
+            green: '#905AFF'
+            yellow: '#685AFF'
+            blue: '#5C78FF'
+            magenta: '#5EA2FF'
+            cyan: '#5AC8FF'
+            white: '#FFFFFF'
 
-				  cursor:
-				    cursor: '#FFFFFF'
-					text: '#1B1B1B'
-_EOF_
+          cursor:
+            cursor: '#FFFFFF'
+            text: '#1B1B1B'
+	_EOF_
 }
 
 # Set compositor configuration
 set_picom_config() {
-		sed -i "$HOME"/.config/bspwm/picom.conf \
-			-e "s/normal = .*/normal =  { fade = false; shadow = false; }/g" \
-			-e "s/shadow-color = .*/shadow-color = \"#000000\"/g" \
-			-e "s/corner-radius = .*/corner-radius = 0/g" \
-			-e "s/\".*:class_g = 'Alacritty'\"/\"95:class_g = 'Alacritty'\"/g" \
-			-e "s/\".*:class_g = 'FloaTerm'\"/\"95:class_g = 'FloaTerm'\"/g"
+	sed -i "$HOME"/.config/bspwm/picom.conf \
+		-e "s/normal = .*/normal =  { fade = false; shadow = false; }/g" \
+		-e "s/shadow-color = .*/shadow-color = \"#000000\"/g" \
+		-e "s/corner-radius = .*/corner-radius = 0/g" \
+		-e "s/\".*:class_g = 'Alacritty'\"/\"95:class_g = 'Alacritty'\"/g" \
+		-e "s/\".*:class_g = 'FloaTerm'\"/\"95:class_g = 'FloaTerm'\"/g"
 }
 
 # Set dunst notification daemon config
 set_dunst_config() {
-		sed -i "$HOME"/.config/bspwm/dunstrc \
-		-e "s/transparency = .*/transparency = 8/g" \
-		-e "s/frame_color = .*/frame_color = \"#1B1B1B\"/g" \
-		-e "s/separator_color = .*/separator_color = \"#2883FF\"/g" \
-		-e "s/font = .*/font = JetBrainsMono Nerd Font Medium 9/g" \
-		-e "s/foreground='.*'/foreground='#2883FF'/g"
+    sed -i "$HOME"/.config/bspwm/dunstrc \
+        -e "s/transparency = .*/transparency = 8/g" \
+        -e "s/frame_color = .*/frame_color = \"#1B1B1B\"/g" \
+        -e "s/separator_color = .*/separator_color = \"#2883FF\"/g" \
+        -e "s/font = .*/font = JetBrainsMono Nerd Font Medium 9/g" \
+        -e "s/foreground='.*'/foreground='#2883FF'/g"
 
-		sed -i '/urgency_low/Q' "$HOME"/.config/bspwm/dunstrc
-		cat >> "$HOME"/.config/bspwm/dunstrc <<- _EOF_
-				[urgency_low]
-				timeout = 3
-				background = "#1B1B1B"
-				foreground = "#F1F1F1"
+    sed -i '/urgency_low/Q' "$HOME"/.config/bspwm/dunstrc
+    cat >> "$HOME"/.config/bspwm/dunstrc <<- _EOF_
+        [urgency_low]
+        timeout = 3
+        background = "#1B1B1B"
+        foreground = "#F1F1F1"
 
-				[urgency_normal]
-				timeout = 6
-				background = "#1B1B1B"
-				foreground = "#F1F1F1"
+        [urgency_normal]
+        timeout = 6
+        background = "#1B1B1B"
+        foreground = "#F1F1F1"
 
-				[urgency_critical]
-				timeout = 0
-				background = "#1B1B1B"
-				foreground = "#F1F1F1"
-_EOF_
+        [urgency_critical]
+        timeout = 0
+        background = "#1B1B1B"
+        foreground = "#F1F1F1"
+	_EOF_
 }
 
 # Launch the bar
 launch_bars() {
-		polybar -q xcad-bar -c ${RICE_DIR}/config.ini &
-		polybar -q xcad-bar2 -c ${RICE_DIR}/config.ini &
-		polybar -q xcad-bar3 -c ${RICE_DIR}/config.ini &
+	polybar -q xcad-bar -c ${RICE_DIR}/config.ini &
+	polybar -q xcad-bar2 -c ${RICE_DIR}/config.ini &
+	polybar -q xcad-bar3 -c ${RICE_DIR}/config.ini &
 }
 
 ### ---------- Apply Configurations ---------- ###
