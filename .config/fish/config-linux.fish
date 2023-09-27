5f87379ca4d891b2914eea2fbaeb1ad2f79d6987
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-09-27 12:26:31 +0700

    🐛 fix(fish): change `exa` to `eza`

--- .config/fish/config-linux.fish
+++ .config/fish/config-linux.fish
@@ -2,7 +2,7 @@ if test -x /home/linuxbrew/.linuxbrew/bin/brew
     eval (/home/linuxbrew/.linuxbrew/bin/brew shellenv)
 end
 
-if type -q exa
-    alias ll "exa -l -g --icons"
+if type -q eza
+    alias ll "eza -l -g --icons"
     alias lla "ll -a"
 end
