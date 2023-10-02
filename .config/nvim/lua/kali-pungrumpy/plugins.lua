e2b88cd3b4d3a0d9d8a10571b2ee6e662b829c97
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-02 08:49:43 +0700

    🐛 fix(nvim): add plugin for `helm` highlight syntax

--- .config/nvim/lua/kali-pungrumpy/plugins.lua
+++ .config/nvim/lua/kali-pungrumpy/plugins.lua
@@ -51,5 +51,5 @@ packer.startup(function(use)
 
   use 'lewis6991/gitsigns.nvim'
   use 'dinhhuy258/git.nvim' -- For git blame & browse
+  use 'towolf/vim-helm'     -- Helm syntax highlighting
 end)
-
