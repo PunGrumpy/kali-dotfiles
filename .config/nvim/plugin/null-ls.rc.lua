4f9dce7ae224660d738db990175393291a6a9ddf
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2023-10-03 08:52:03 +0700

    🐛 fix(nvim): update new go plugin

--- .config/nvim/plugin/null-ls.rc.lua
+++ .config/nvim/plugin/null-ls.rc.lua
@@ -15,7 +15,9 @@ end
 null_ls.setup {
   sources = {
     null_ls.builtins.formatting.prettierd,
-    null_ls.builtins.formatting.golines,
+    null_ls.builtins.formatting.gofmt,
+    -- null_ls.builtins.formatting.gofumpt, -- gofumpt is stricted than gofmt
+    null_ls.builtins.formatting.goimports,
     null_ls.builtins.formatting.terraform_fmt,
     null_ls.builtins.formatting.fish_indent,
     null_ls.builtins.formatting.prismaFmt,
@@ -24,11 +26,14 @@ null_ls.setup {
     }),
     null_ls.builtins.diagnostics.fish,
     null_ls.builtins.diagnostics.golangci_lint,
+    null_ls.builtins.diagnostics.staticcheck,
     null_ls.builtins.diagnostics.hadolint,
     null_ls.builtins.diagnostics.terraform_validate,
     null_ls.builtins.diagnostics.tfsec,
     null_ls.builtins.diagnostics.ansiblelint,
     null_ls.builtins.diagnostics.actionlint,
+    null_ls.builtins.code_actions.gomodifytags,
+    null_ls.builtins.code_actions.impl,
   },
   on_attach = function(client, bufnr)
     if client.supports_method("textDocument/formatting") then
