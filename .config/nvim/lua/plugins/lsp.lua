7be60397bad44e71c8f77bb3bed9dca36a3f2889
PunGrumpy <108584943+PunGrumpy@users.noreply.github.com>
2024-08-22 22:06:42 +0700

    fix(neovim): lsp should not reuse windows when running 'Go to Definition'

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -149,4 +149,21 @@ return {
 			setup = {},
 		},
 	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = function()
+			local keys = require("lazyvim.plugins.lsp.keymaps").get()
+			vim.list_extend(keys, {
+				{
+					"gd",
+					function()
+						-- DO NOT RESUSE WINDOW
+						require("telescope.builtin").lsp_definitions({ reuse_win = false })
+					end,
+					desc = "Goto Definition",
+					has = "definition",
+				},
+			})
+		end,
+	},
 }
